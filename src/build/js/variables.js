ba06614d42b6f9e5f89f740665c68bfcabe8b120
MohamedSohel <MohamedSohel@users.noreply.github.com>
2023-07-03 11:32:44 +0000

    Update Tokens

--- src/build/js/variables.js
+++ src/build/js/variables.js
@@ -1,18 +1,18 @@
 /**
  * Do not edit directly
- * Generated on Mon, 03 Jul 2023 11:29:52 GMT
+ * Generated on Mon, 03 Jul 2023 11:32:44 GMT
  */
 
 export const ColorsPrimary = "#000000";
 export const ColorsSecondary = "#ffffff";
-export const ColorsTertiary = "#e80707";
-export const FontSizesH1 = 48.829;
-export const FontSizesH2 = 39.063;
-export const FontSizesH3 = 31.25;
-export const FontSizesH4 = 25;
-export const FontSizesH5 = 20;
-export const FontSizesH6 = 16;
-export const FontSizesBody = 16;
-export const FontSizesSm = 13.6;
-export const FontSizesXs = 10.4;
+export const ColorsTertiary = "#00ff00";
+export const FontSizesH1 = 61.036;
+export const FontSizesH2 = 48.829;
+export const FontSizesH3 = 39.063;
+export const FontSizesH4 = 31.25;
+export const FontSizesH5 = 25;
+export const FontSizesH6 = 20;
+export const FontSizesBody = 20;
+export const FontSizesSm = 17;
+export const FontSizesXs = 13;
 export const TokenSetOrder0 = "core";
