ddbde1298199511fa690ff88a3795bc047941ac1
Sohel-PS <mohamed.sohel@publicissapient.com>
2023-07-03 15:59:26 +0530

    added github action

--- src/build/js/variables.js
+++ src/build/js/variables.js
@@ -1,17 +1,18 @@
 /**
  * Do not edit directly
- * Generated on Mon, 03 Jul 2023 08:29:10 GMT
+ * Generated on Mon, 03 Jul 2023 10:25:37 GMT
  */
 
 export const ColorsPrimary = "#000000";
 export const ColorsSecondary = "#ffffff";
-export const ColorsTertiary = "#E76D15";
-export const FontSizesH1 = "16 * 1 * 1.25 * 1.25 * 1.25 * 1.25 * 1.25";
-export const FontSizesH2 = "16 * 1 * 1.25 * 1.25 * 1.25 * 1.25";
-export const FontSizesH3 = "16 * 1 * 1.25 * 1.25 * 1.25";
-export const FontSizesH4 = "16 * 1 * 1.25 * 1.25";
-export const FontSizesH5 = "16 * 1 * 1.25";
-export const FontSizesH6 = "16 * 1";
-export const FontSizesBody = "16";
-export const FontSizesSm = "16 * 0.85";
-export const FontSizesXs = "16 * 0.65";
+export const ColorsTertiary = "#e80707";
+export const FontSizesH1 = 48.829;
+export const FontSizesH2 = 39.063;
+export const FontSizesH3 = 31.25;
+export const FontSizesH4 = 25;
+export const FontSizesH5 = 20;
+export const FontSizesH6 = 16;
+export const FontSizesBody = 16;
+export const FontSizesSm = 13.6;
+export const FontSizesXs = 10.4;
+export const TokenSetOrder0 = "core";
